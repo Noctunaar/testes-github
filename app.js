2842d3e71c6ec4ceb4c805d7cf9632d63854e76e
Bruna Luíza <bluiza317@gmail.com>
2025-02-10 17:20:36 -0300

    teste: facilitando o game

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 10;
 let numeroSecreto = geraNumeroAleatorio();
 let tentativas = 1;
 
@@ -25,7 +25,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Testes Github');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
